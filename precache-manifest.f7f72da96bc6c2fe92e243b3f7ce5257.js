8d260cb08fe6261157b8e1ec8f0c506da80b7573
Amir <nik.amirsa@gmail.com>
2020-05-26 20:19:45 +0430

    Updates

---
+++ precache-manifest.f7f72da96bc6c2fe92e243b3f7ce5257.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0b85c76fcd1fe75149f4736eba490f",
+    "url": "/fake-array-generator/index.html"
+  },
+  {
+    "revision": "276afbfcc2f54c560bfb",
+    "url": "/fake-array-generator/static/css/2.c20f45dc.chunk.css"
+  },
+  {
+    "revision": "7bd292bf4539dfe47eaa",
+    "url": "/fake-array-generator/static/css/main.0688b10c.chunk.css"
+  },
+  {
+    "revision": "276afbfcc2f54c560bfb",
+    "url": "/fake-array-generator/static/js/2.536350f2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/fake-array-generator/static/js/2.536350f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd292bf4539dfe47eaa",
+    "url": "/fake-array-generator/static/js/main.6deaaf44.chunk.js"
+  },
+  {
+    "revision": "fbb63f6e9c00b36242e6",
+    "url": "/fake-array-generator/static/js/runtime-main.e1fe3730.js"
+  },
+  {
+    "revision": "6011079c55822b89e996df7543e20d84",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-300.6011079c.woff"
+  },
+  {
+    "revision": "9228cb37d42e82d7738ee1843ae98f63",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-300.9228cb37.woff2"
+  },
+  {
+    "revision": "5cb8925d8c45787b80a01f51020d8ab8",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-400.5cb8925d.woff2"
+  },
+  {
+    "revision": "ac2c653a7f6f7433930bdbfeb6de7b6d",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-400.ac2c653a.woff"
+  },
+  {
+    "revision": "695a61235d1fbdf6fb1c13d89ed2d9f0",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-500.695a6123.woff"
+  },
+  {
+    "revision": "acc2d63d4a57a32d6d8a0bb1c6cf1e8d",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-500.acc2d63d.woff2"
+  },
+  {
+    "revision": "9be0d2f1e9006f77a5b5e2d49227290e",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-600.9be0d2f1.woff"
+  },
+  {
+    "revision": "f40c2f90e9bc0b80d908586c419e7e4d",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-600.f40c2f90.woff2"
+  },
+  {
+    "revision": "86f1910426a7c069e1f4e13ae19372d5",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-700.86f19104.woff"
+  },
+  {
+    "revision": "c86d300fdf3b5ad894f37f07035724d5",
+    "url": "/fake-array-generator/static/media/comfortaa-latin-700.c86d300f.woff2"
+  }
+]);
