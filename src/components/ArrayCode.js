31ed53ddd9e7570b052613892ccad02bb0a8b131
Amir <nik.amirsa@gmail.com>
2020-05-26 11:04:18 +0430

    generate id

--- src/components/ArrayCode.js
+++ src/components/ArrayCode.js
@@ -3,30 +3,14 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import MainContext from '../context/mainContext';
 
-const sss = `
-[
-    {
-        id: 0,
-        title: 'salam'
-    },
-    {
-        id: 0,
-        title: 'salam'
-    },
-    {
-        id: 0,
-        title: 'salam'
-    },
-]
-`;
-
 const ArrayCode = () => {
-  const { arrayLength } = useContext(MainContext);
+  const { arrayLength, autoID, randomId } = useContext(MainContext);
 
   const generateArray = () => {
     let str = '';
     for (let i = 0; i < arrayLength; i++) {
       str += `{ 
+        id: ${autoID ? i + 1 : Math.random()},
         title: 'salam'
       },
       `;
