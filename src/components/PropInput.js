8b9cb11ff820c959d1090dc255f78f81afc6cb4b
Amir Salehi <nik.amirsa@gmail.com>
2021-12-05 08:38:56 +0330

    delete prop handler

--- src/components/PropInput.js
+++ src/components/PropInput.js
@@ -21,6 +21,10 @@ export default function PropInput({ data }) {
         });
     }, [keyValue, valueValue]);
 
+    const handleDeleteProp = () => {
+        console.log(id)
+    }
+
     return (
         <div className="record">
             <input type="text"
@@ -35,12 +39,14 @@ export default function PropInput({ data }) {
                 onChange={(e) => setValueValue(e.target.value)}
             />
 
-            <button style={{
-                border: 'none',
-                background: 'transparent',
-                cursor: 'pointer',
-                fill: '#fff',
-            }}>
+            <button
+                onClick={handleDeleteProp}
+                style={{
+                    border: 'none',
+                    background: 'transparent',
+                    cursor: 'pointer',
+                    fill: '#fff',
+                }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" /><path d="M0 0h24v24H0z" fill="none" /></svg>
             </button>
